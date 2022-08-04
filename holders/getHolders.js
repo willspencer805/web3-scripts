8b5d1afd3a0105567aedec4aaa483eac81ca5815
Will Spencer <Will.Spencer@MacBook-Air.local>
2022-08-04 15:38:03 -0400

    updated references to secret keys

--- holders/getHolders.js
+++ holders/getHolders.js
@@ -1,66 +1,64 @@
-const axios = require("axios").default;
-const ethers = require("ethers");
-const {
-  tokenIds,
-  addresses,
-  alchemyKey,
-  ethAlchemyKey,
-} = require("./variables.js");
-const fs = require("fs/promises");
+const axios = require("axios").default
+const ethers = require("ethers")
+const { tokenIds, addresses } = require("./holderVariables.js")
+const fs = require("fs/promises")
+require("dotenv").config({ path: require("find-config")(".env") })
 
 const main = async () => {
+  // grab .env vars
+  const alchemyPolygonKey = process.env.ALCHEMY_POLYGON_KEY
+  const alchemyEthKey = process.env.ALCHEMY_ETH_KEY
+
   // initialize headers for csv file
-  await fs.appendFile("./numberHeldByAddress.csv", "address,numHeld\n");
-  const openseaContract = "0x2953399124F0cBB46d2CbACD8A89cF0599974963";
-  const baseURL = `https://polygon-mainnet.g.alchemyapi.io/v2/${alchemyKey}/getNFTs/`;
+  await fs.appendFile("./numberHeldByAddress.csv", "address,numHeld\n")
+  const openseaContract = "0x2953399124F0cBB46d2CbACD8A89cF0599974963" // Opensea ERC1155 contract
+  const baseURL = `https://polygon-mainnet.g.alchemyapi.io/v2/${alchemyPolygonKey}/getNFTs/`
 
   // initialize provider to resovle ens names
-  const ethProvider = new ethers.providers.AlchemyProvider(
-    (network = "homestead"),
-    ethAlchemyKey
-  );
+  const ethProvider = new ethers.providers.AlchemyProvider((network = "homestead"), alchemyEthKey)
 
   // loop over all the addresses
   for (i = 0; i < addresses.length; i++) {
+    await fs.appendFile("./numberHeldByAddress.csv", `${addresses[i]},`)
+
+    // resolve ens names to address
     if (addresses[i].slice(-4) == ".eth") {
-      addresses[i] = await ethProvider.resolveName(addresses[i]);
+      addresses[i] = await ethProvider.resolveName(addresses[i])
     }
 
+    // build request body
     const config = {
       method: "get",
       url: `${baseURL}?owner=${addresses[i]}&contractAddresses[]=${openseaContract}`,
-    };
+    }
 
     try {
-      // query alchemy node for all token transactions
-      let response = await axios(config);
+      // query alchemy nft api for tokens owned by address
+      let response = await axios(config)
       if (response) {
-        response = response.data.ownedNfts;
+        response = response.data.ownedNfts
       }
 
-      let numHeld = 0;
+      let numHeld = 0
 
-      // loop over number of ERC1155 transactions for each asset
+      // loop over number of ERC1155 tokens owned by address
       for (j = 0; j < response.length; j++) {
-        let id = response[j].id.tokenId;
+        let id = response[j].id.tokenId
 
         // check if token id returned is in the list of blockchain guild token ids
         if (tokenIds.includes(id.toUpperCase())) {
-          numHeld++;
+          numHeld++
         }
       }
-      console.log(`${addresses[i]} holds ${numHeld} blockchain guild tokens`);
-
       // write to csv file
-      await fs.appendFile(
-        "./numberHeldByAddress.csv",
-        `${addresses[i]},${numHeld}\n`
-      );
+      await fs.appendFile("./numberHeldByAddress.csv", `${numHeld}\n`)
+
+      console.log(`${addresses[i]} holds ${numHeld} blockchain guild tokens`)
     } catch (error) {
-      console.log(error);
-      fs.appendFile("./numberHeldByAddress.csv", `${addresses[i]},${null}\n`);
+      console.log(error)
+      fs.appendFile("./numberHeldByAddress.csv", `${null}\n`)
     }
   }
-};
+}
 
-main();
+main()
